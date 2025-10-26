5a3f9d7fa69dd1588d30875711e9d9214792b7f9
awesoma31 <gwerty31102005@gmail.com>
2025-10-26 23:03:49 +0300

    increase test timeout

--- ci/test/suite/custom.py
+++ ci/test/suite/custom.py
@@ -212,7 +212,7 @@ ALLOCTEST = SimpleSuite(
     tests = [
         PatternTest(
             name = "filetest",
-            timeout = timedelta(seconds = 2),
+            timeout = timedelta(seconds = 600),
             patterns = [
                 "filetest: start",
                 "filetest: OK",
