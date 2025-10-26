8913daaaf9e92d61c74e559c779781f30d32cf01
awesoma31 <gwerty31102005@gmail.com>
2025-10-26 23:27:26 +0300

    delete kjhgfds

--- ci/test/xv6.py
+++ ci/test/xv6.py
@@ -1,6 +1,5 @@
 from stream import RWStream
 from test import assert_eq
-import time
 
 
 class Xv6:
@@ -8,7 +7,6 @@ class Xv6:
         self.stream = stream
 
     def run(self, program: str):
-        time.sleep(0.1)
         self.stream.writeline(program)
         assert_eq(self.stream.readline(), program)
         assert_eq(self.stream.readline(), "")
